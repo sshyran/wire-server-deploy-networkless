b5f1af60acc663c23d7feb6c2567553ff45cfdc1
Julia Longtin <julia.longtin@wire.com>
2019-06-01 16:51:31 +0100

    reorder

--- kvmhelpers/start_kvm.sh
+++ kvmhelpers/start_kvm.sh
@@ -51,6 +51,53 @@ if [ -n "${DISPLAY+x}" ]; then
     fi
 fi
 
+
+# paths to binaries we use.
+IP="/sbin/ip"
+SUDO="/usr/bin/sudo"
+WHOAMI="/usr/bin/whoami"
+GREP="/bin/grep"
+WC="/usr/bin/wc"
+SEQ="/usr/bin/seq"
+SORT="/usr/bin/sort"
+TAIL="/usr/bin/tail"
+SED="/bin/sed"
+
+# The user who is running this script.
+USER=$(${WHOAMI})
+
+# Claim a tap device, and use it.
+function claim_tap() {
+    
+    TAPDEVS=$($IP tuntap | $GREP -E ^tap | $SED "s/:.*//")
+    TAPDEVCOUNT=$(echo -n "$TAPDEVS" | $WC -l)
+    # First, try to fill in any gaps.
+    LASTTAP=$(echo -n "$TAPDEVS" | $SED "s/t..//" | $SORT -g | $TAIL -n 1)
+    for each in $($SEQ 0 $LASTTAP); do
+	if [ $(($TAPSTRIED + $TAPDEVCOUNT)) == $LASTTAP ]; then
+	    break
+	fi
+	if [ -z "$($IP tuntap | $GREP -E ^tap$each)" ]; then
+	    $SUDO $IP tuntap add dev tap$each mode tap user $USER
+	    if [ $? -eq 0 ]; then
+		echo tap$each
+		return 0
+	    fi
+	    TAPSTRIED=$(($TAPSTRIED+1))
+	fi
+    done
+
+    # Then, try to claim one on the end. up to 99
+    for each in $($SEQ $(($LASTTAP+1)) 99); do
+	$SUDO $IP tuntap add dev tap$each mode tap user $USER
+	if [ $? -eq 0 ]; then
+	    echo tap$each
+	    return 0
+	fi
+    done
+}
+
+# set up networking.
 for each in ${!eth*}; do
     TAPDEV=$(claim_tap)
     ASSIGNED_TAPS="$ASSIGNED_TAPS $TAPDEV"
@@ -83,53 +130,4 @@ for each in $ASSIGNED_TAPS; do
 done
 
 #### you should not have to modify these. tell the author if you have to. ####
-
-# paths to binaries we use.
-IP="/sbin/ip"
-SUDO="/usr/bin/sudo"
-WHOAMI="/usr/bin/whoami"
-GREP="/bin/grep"
-WC="/usr/bin/wc"
-SEQ="/usr/bin/seq"
-SORT="/usr/bin/sort"
-TAIL="/usr/bin/tail"
-SED="/bin/sed"
-
-# The user who is running this script.
-USER=$(${WHOAMI})
-
-function try_to_acquire () {
-	$SUDO $IP tuntap add dev $1 mode tap user $USER
-	return $?
-}
-
-function claim_tap() {
-    
-    TAPDEVS=$($IP tuntap | $GREP -E ^tap | $SED "s/:.*//")
-    TAPDEVCOUNT=$(echo -n "$TAPDEVS" | $WC -l)
-    # First, try to fill in any gaps.
-    LASTTAP=$(echo -n "$TAPDEVS" | $SED "s/t..//" | $SORT -g | $TAIL -n 1)
-    for each in $($SEQ 0 $LASTTAP); do
-	if [ $(($TAPSTRIED + $TAPDEVCOUNT)) == $LASTTAP ]; then
-	    break
-	fi
-	if [ -z "$($IP tuntap | $GREP -E ^tap$each)" ]; then
-	    try_to_acquire tap$each
-	    if [ $? -eq 0 ]; then
-		echo tap$each
-		return 0
-	    fi
-	    TAPSTRIED=$(($TAPSTRIED+1))
-	fi
-    done
-
-    # Then, try to claim one on the end. up to 99
-    for each in $($SEQ $(($LASTTAP+1)) 99); do
-	try_to_acquire tap$each 
-	if [ $? -eq 0 ]; then
-	    echo tap$each
-	    return 0
-	fi
-    done
-}
 
