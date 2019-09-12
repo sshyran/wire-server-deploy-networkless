2227827c72b810404204dd5e89f9df0a37d882cb
jschaul <jschaul@users.noreply.github.com>
2019-09-12 18:14:19 +0200

    add comment

--- download.sh
+++ download.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+#TODO: this script relies on spoof-ssl having run (Otherwise the sed query below doesn't work), but also on spoof-dns NOT yet having run (otherwise the download doesn't work). TODO re-shuffle code around.
+
 set -ex
 #USAGE="$0 <domain name>"
-
 #DOMAINNAME=${1:?$USAGE}
+
+export DOMAINNAME=raw.githubusercontent.com
 
 APACHE_ROOT=/opt/offline
 
@@ -16,7 +19,6 @@ mkdir -p $DIR_SUB
 curl https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py -o $DIR_SUB/get-poetry.py
 chown -R www-data.www-data $APACHE_ROOT
 
-export DOMAINNAME=raw.githubusercontent.com
 sudo sed -i "s=\(</VirtualHost>\)=\tServerName $DOMAINNAME\n\t\tHostnameLookups Off\n\t\talias /$DIRNAME $TARGETDIR\n\t\t<Directory $TARGETDIR>\n\t\t\tOptions Indexes FollowSymLinks MultiViews\n\t\t\tRequire all granted\n\t\t</Directory>\n\t\1=" /etc/apache2/sites-available/000-$DOMAINNAME.conf
 
 sudo service apache2 restart
