d5c069f41cc02b00c35031ec1f0b5d99aecbec1c
Julia Longtin <julia.longtin@wire.com>
2020-02-12 19:37:28 +0000

    add prefix to pushes.

--- vpc/ansible/files/registry/upload_image.sh
+++ vpc/ansible/files/registry/upload_image.sh
@@ -5,11 +5,11 @@ registry_name="localhost:5001"
 function mirror() {
     prefix=$1
     image=$2
-    rmafter=$3
+    rmafter=$4
     docker pull $prefix/$image
-    docker tag $prefix/$image $registry_name/$image
-    docker push $registry_name/$image
-    [ -n "$rmafter" ] && docker image remove $registry_name/$image
+    docker tag $prefix/$image $registry_name/$prefix/$image
+    docker push $registry_name/$prefix/$image
+    [ -n "$rmafter" ] && docker image remove $registry_name/$prefix/$image
     [ -n "$rmafter" ] && docker image remove $prefix/$image
 }
 
