75686d9ecc50eb7daf08fe86e11901cd6cead139
Gregor <gregor.jahn@wire.com>
2020-02-21 14:38:14 +0100

    Allow to delete images form registry via REST API
    * this env var is just the foundation
    * to actually make our life easier, we would need sth along those lines: https://gist.github.com/jaytaylor/86d5efaddda926a25fa68c263830dac1
    * and, to round it all up: invoke `registry garbage-collect`
    
    thank goodness, they never figured out a business model for that shitty piece of crapware

--- vpc/ansible/roles/serve_docker_repo/files/registry/registry-run.sh
+++ vpc/ansible/roles/serve_docker_repo/files/registry/registry-run.sh
@@ -10,4 +10,5 @@ docker run \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:5001 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/client.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/client.key \
+  -e REGISTRY_STORAGE_DELETE_ENABLED=true \
   registry:2
